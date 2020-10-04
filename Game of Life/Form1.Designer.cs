d1521aa97235742030c7035220d825454cf99c92
Shiwei Pan <1923437477@qq.com>
2020-10-04 13:48:53 +0800

    game of live_v1

---
+++ Game of Life/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Game_of_Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rownum = new System.Windows.Forms.NumericUpDown();
+            this.columnnum = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.rownum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columnnum)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("华文中宋", 15F);
+            this.button1.Location = new System.Drawing.Point(366, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "改变大小";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("华文中宋", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(23, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "行";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("华文中宋", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(180, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 40);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "列";
+            // 
+            // rownum
+            // 
+            this.rownum.Font = new System.Drawing.Font("MathJax_Main", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rownum.Location = new System.Drawing.Point(76, 28);
+            this.rownum.Name = "rownum";
+            this.rownum.Size = new System.Drawing.Size(98, 31);
+            this.rownum.TabIndex = 5;
+            this.rownum.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.rownum.ValueChanged += new System.EventHandler(this.rownum_ValueChanged);
+            // 
+            // columnnum
+            // 
+            this.columnnum.Font = new System.Drawing.Font("MathJax_Main", 12F);
+            this.columnnum.Location = new System.Drawing.Point(228, 28);
+            this.columnnum.Name = "columnnum";
+            this.columnnum.Size = new System.Drawing.Size(98, 31);
+            this.columnnum.TabIndex = 6;
+            this.columnnum.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.columnnum.ValueChanged += new System.EventHandler(this.columnnum_ValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("华文中宋", 15F);
+            this.button2.Location = new System.Drawing.Point(512, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "单步";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("华文中宋", 15F);
+            this.button3.Location = new System.Drawing.Point(642, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 46);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "自动";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("华文中宋", 15F);
+            this.button4.Location = new System.Drawing.Point(770, 16);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 46);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "停";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 490);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.columnnum);
+            this.Controls.Add(this.rownum);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.rownum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.columnnum)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown rownum;
+        private System.Windows.Forms.NumericUpDown columnnum;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Game of Life/Form1.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Game_of_Life
+{
+    public partial class Form1 : Form
+    {
+        int Row = 5;
+        int Column = 20;
+        public int[,] pos = new int[20, 5];
+        public string[] btnlist = new string[] { "button2", "button1","button3", "button4" };
+        public int[,] tmp = new int[20, 5];
+
+        public Form1()
+        {
+            InitializeComponent();
+            refresh();
+        }   
+                    
+
+        public void refresh()         
+        {
+            int x = 10;
+            int y = 30;//初始位置
+            for (int i = 0; i < Row * Column; i++)
+            {
+                if (i % Column == 0)
+                {
+                    x = 10;
+                    y += 30;
+                }
+                Button NewButton = new Button();
+                NewButton.Location = new Point(x, y);
+                NewButton.Size = new Size(30, 30);
+                NewButton.Click += NewButton_Click;
+                this.Controls.Add(NewButton);
+                x += 30;
+            }
+    
+        }
+
+        public void Restart()
+        {
+            int sum = Column * Row;
+            while (sum > 0.1)
+            {
+                sum = sum / 2;
+
+                foreach (Control ctl in this.Controls)
+                {
+                    bool ok = true;
+                    if (ctl is Button)
+                    {
+                        foreach (string name in btnlist)
+                        {
+                            if (name == ctl.Name.ToString())
+                                ok = false;
+                        }
+                        if (ok)
+                            this.Controls.Remove(ctl);
+                    }
+                }
+            }
+            int x = 10;
+            int y = 30;//初始位置
+            for (int i = 0; i < Row * Column; i++)
+            {
+                if (i % Column == 0)
+                {
+                    x = 10;
+                    y += 30;
+                }
+               
+                Button NewButton = new Button();
+                NewButton.Location = new Point(x, y);
+                NewButton.Size = new Size(30, 30);
+                NewButton.Click += NewButton_Click;
+                this.Controls.Add(NewButton);
+                if (tmp[(x - 10) / 30, ((y - 30) / 30 )- 1] == 1)
+                    NewButton.BackColor = Color.Blue;
+                x += 30;
+            }
+
+        }
+
+        private void NewButton_Click(object sender, EventArgs e)
+        {
+            int px,py;
+            Button p = (Button)sender;
+            p.BackColor = Color.Blue;
+            px = (p.Location.X - 10) / 30;
+            py = (p.Location.Y - 30) / 30 -1;
+            Console.Write(px+" ");
+            Console.Write(py);
+            Console.WriteLine("\n");
+            pos[px, py] = 1;
+        }
+
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void columnnum_ValueChanged(object sender, EventArgs e)
+        {
+            Column=(int)columnnum.Value;
+        }
+
+        private void rownum_ValueChanged(object sender, EventArgs e)
+        {
+            Row = (int)rownum.Value;
+        }
+
+        //更新大小
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pos = new int[Column, Row];
+            int sum = Column * Row;
+            while (sum > 0.1)
+            {
+                sum = sum / 2;
+         
+                foreach (Control ctl in this.Controls)
+                {
+                    bool ok = true;
+                    if (ctl is Button)
+                    {
+                        foreach(string name in btnlist)
+                        {
+                            if (name == ctl.Name.ToString())
+                                ok = false;
+                        }
+                        if(ok)
+                            this.Controls.Remove(ctl);
+                    }
+                }
+            }
+          
+            refresh();
+        }
+
+  
+
+        private static bool Equal(int s)
+        {
+            if (s == 1)
+                return true;
+            else
+                return false;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Console.WriteLine("ori");
+            for (int j = 0; j < Row; j++)
+            {
+                for (int i = 0; i < Column; i++)
+                    Console.Write(pos[i, j] + " ");
+                Console.WriteLine("\n");
+
+            }
+            tmp = new int[Column, Row];
+            for (int m = 0; m < Column; m++)
+                for (int n = 0; n < Row; n++)
+                {
+                    if (pos[m, n] == 1)
+                    {
+                        int s = 0;
+                        for(int p=0;p<3;p++)
+                            for(int q = 0; q < 3; q++)
+                            {
+                                if (m + p - 1 >= Column || m + p - 1 < 0)
+                                    continue;
+                                else if (n + q - 1 >= Row || n + q - 1 < 0)
+                                    continue;
+                                else
+                                    s += pos[m + p - 1, n + q - 1];
+                            }
+                        if (s > 2 && s < 5)
+                            tmp[m, n] = 1;
+                    }
+                    else
+                    {
+                        int s = 0;
+                        for (int p = 0; p < 3; p++)
+                            for (int q = 0; q < 3; q++)
+                            {
+                                if (m + p - 1 >= Column || m + p - 1 < 0)
+                                    continue;
+                                else if (n + q - 1 >= Row || n + q - 1 < 0)
+                                    continue;
+                                else
+                                    s += pos[m + p - 1, n + q - 1];
+                            }
+                        if (s==3)
+                            tmp[m, n] = 1;
+                    }
+                }
+            Console.WriteLine("tmp");
+            for (int j = 0; j < Row; j++)
+            {
+                for (int i = 0; i < Column; i++)
+                {
+                    Console.Write(tmp[i, j] + " ");
+                    pos[i, j] = tmp[i, j];
+                }
+                Console.WriteLine("\n");
+            }
+
+            int sum = Column * Row;//清除原图
+            while (sum > 0.1)
+            {
+                sum = sum / 2;
+
+                foreach (Control ctl in this.Controls)
+                {
+                    bool ok = true;
+                    if (ctl is Button)
+                    {
+                        foreach (string name in btnlist)
+                        {
+                            if (name == ctl.Name.ToString())
+                                ok = false;
+                        }
+                        if (ok)
+                            this.Controls.Remove(ctl);
+                    }
+                }
+            }
+      
+            Restart();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine("ori");
+            for (int j = 0; j < Row; j++)
+            {
+                for (int i = 0; i < Column; i++)
+                    Console.Write(pos[i, j] + " ");
+                Console.WriteLine("\n");
+
+            }
+            tmp = new int[Column, Row];
+            for (int m = 0; m < Column; m++)
+                for (int n = 0; n < Row; n++)
+                {
+                    if (pos[m, n] == 1)
+                    {
+                        int s = 0;
+                        for (int p = 0; p < 3; p++)
+                            for (int q = 0; q < 3; q++)
+                            {
+                                if (m + p - 1 >= Column || m + p - 1 < 0)
+                                    continue;
+                                else if (n + q - 1 >= Row || n + q - 1 < 0)
+                                    continue;
+                                else
+                                    s += pos[m + p - 1, n + q - 1];
+                            }
+                        if (s > 2 && s < 5)
+                            tmp[m, n] = 1;
+                    }
+                    else
+                    {
+                        int s = 0;
+                        for (int p = 0; p < 3; p++)
+                            for (int q = 0; q < 3; q++)
+                            {
+                                if (m + p - 1 >= Column || m + p - 1 < 0)
+                                    continue;
+                                else if (n + q - 1 >= Row || n + q - 1 < 0)
+                                    continue;
+                                else
+                                    s += pos[m + p - 1, n + q - 1];
+                            }
+                        if (s == 3)
+                            tmp[m, n] = 1;
+                    }
+                }
+            Console.WriteLine("tmp");
+            for (int j = 0; j < Row; j++)
+            {
+                for (int i = 0; i < Column; i++)
+                {
+                    Console.Write(tmp[i, j] + " ");
+                    pos[i, j] = tmp[i, j];
+                }
+                Console.WriteLine("\n");
+            }
+
+            int sum = Column * Row;//清除原图
+            while (sum > 0.1)
+            {
+                sum = sum / 2;
+
+                foreach (Control ctl in this.Controls)
+                {
+                    bool ok = true;
+                    if (ctl is Button)
+                    {
+                        foreach (string name in btnlist)
+                        {
+                            if (name == ctl.Name.ToString())
+                                ok = false;
+                        }
+                        if (ok)
+                            this.Controls.Remove(ctl);
+                    }
+                }
+            }
+      
+            Restart();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+    }
+}
